8736237795276bb567f827223f24a3e4f59d372a
Tyler Clemens <tyler.r.clemens@gmail.com>
2023-05-20 09:58:42 -0700

    Update Image

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule ReplaceWithTest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:image, "~> 0.31.0"},
-      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:image, "~> 0.31.1"},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
